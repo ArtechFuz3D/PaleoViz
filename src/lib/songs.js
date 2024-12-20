62eb68a6f46752a74d016202d6aac9781a41cf53
ArtechFuz3D-Studio <72121107+ArtechFuz3D-Studio@users.noreply.github.com>
2024-12-20 20:45:23 +0200

    legoletsgo

--- src/lib/songs.js
+++ src/lib/songs.js
@@ -1,10 +1,13 @@
 const playlist = [
+  "/tracks/gazzara24.mp3",
     "/tracks/nirvana.mp3",
     "/tracks/seagulls40.mp3",
     "/tracks/rendezvouz.mp3",
     "/tracks/youdontbringmeflowers.mp3",
     "/tracks/dointime.mp3",
-    "/tracks/gazzara24",
+    "/tracks/lamericano.mp3",
+    "/tracks/worldholdon.mp3",
+    "/tracks/aeroplane.mp3",
     "/tracks.lost.mp3"
   ];
   
